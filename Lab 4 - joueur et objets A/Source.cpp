4fa80d755f9568921aab8245c1688f3feecc519e
Bettina Janesch <e.bsjanesch@etu.cvm.qc.ca>
2023-09-18 19:11:22 -0400

    SHow player stats + menu made - all edge cases work. final commit

--- Lab 4 - joueur et objets A/Source.cpp
+++ Lab 4 - joueur et objets A/Source.cpp
@@ -28,8 +28,8 @@ struct Player_s
 
 void AjouterActionsetCredits(Player_s &Player);
 void AchatObjet(vector <Inventaire_s>& marchand, int& indexvectormarchand, int& QuantitePourAcheter, Player_s& Player);
-
 void UtiliserObjet(Player_s& Player, int& indexvectorplayer, int& QuantitePourUtiliser);
+void AfficherJoueur(Player_s& Player);
 
 void main(void)
 {
@@ -39,39 +39,50 @@ void main(void)
 
 	vector <Inventaire_s> Marchand = { {{"Pelle", 10000, "Semer légumes"},100}, {{"Arrosoir (1L)", 5000, "Arroser cultures"},100},{{"Grain (1KG)", 6000, "Nourrir animaux"},100} };
 
-	cout << "MARCHAND: INVENTAIRE D'OBJETS\n";
-	for (int i = 0; i < 3; i++)
-	{
-		cout << Marchand[i].Objet.NomObjet << "\t" << Marchand[i].Objet.CreditCost << "\t" << Marchand[i].Objet.Capacite << "\tQuantite disponible: " << Marchand[i].QuantiteObjet << "\n";
-	}
-
+	int OptionMenu;
 	int QuantiteDesireAchat;
 	int IndexDesireAchat;
 	int QuantiteDesireUtiliser;
 	int IndexDesireUtiliser;
 
+	do
+	{
+		cout << "Quoi faire? 1. Acheter 2. Utiliser 3. Quit";
+		_getch();
+		cin >> OptionMenu;
 
-	cout << "\nQuel objet a acheter (0,1,2)";
-	cin >> IndexDesireAchat;
-	cout << "\nCombien d'objets a acheter";
-	cin >> QuantiteDesireAchat;
-	AchatObjet(Marchand, IndexDesireAchat, QuantiteDesireAchat, Joueur);
-	cout << "\n\nCredits joueur: " << Joueur.Credits << "\nName + quantity: ";
-	cout << Joueur.InventaireJoueur[0].Objet.NomObjet << Joueur.InventaireJoueur[0].QuantiteObjet;
-	cout << "\n\nQuel objet a utiliser? (0,1)";
-	cin >> IndexDesireUtiliser;
-	cout << "\nCombien a utiliser";
-	cin >> QuantiteDesireUtiliser;
-	UtiliserObjet(Joueur, IndexDesireUtiliser, QuantiteDesireUtiliser);
-	cout << "\n\nCredits joueur: " << Joueur.Credits << "\nName + quantity: ";
-	cout << Joueur.InventaireJoueur[0].Objet.NomObjet << Joueur.InventaireJoueur[0].QuantiteObjet;
+		if (OptionMenu == 1) // Achat
+		{
+			cout << "MARCHAND: INVENTAIRE D'OBJETS\n";
+			for (int i = 0; i < 3; i++)
+			{
+				cout << Marchand[i].Objet.NomObjet << "\t" << Marchand[i].Objet.CreditCost << "\t" << Marchand[i].Objet.Capacite << "\tQuantite disponible: " << Marchand[i].QuantiteObjet << "\n";
+			}
+			cout << "\nQuel objet a acheter (0,1,2)";
+			cin >> IndexDesireAchat;
+			cout << "\nCombien d'objets a acheter";
+			cin >> QuantiteDesireAchat;
+			AchatObjet(Marchand, IndexDesireAchat, QuantiteDesireAchat, Joueur);
+			AfficherJoueur(Joueur);
+		}
 
+		else if (OptionMenu == 2)
+		{
+			AfficherJoueur(Joueur);
+			cout << "\n\nQuel objet a utiliser? (0,1)";
+			cin >> IndexDesireUtiliser;
+			cout << "\nCombien a utiliser";
+			cin >> QuantiteDesireUtiliser;
+			UtiliserObjet(Joueur, IndexDesireUtiliser, QuantiteDesireUtiliser);
+			AfficherJoueur(Joueur);
+		}
 
-
-
-
-
-
+		else
+		{
+			_getch();
+		}
+	}
+	while (OptionMenu != 3);
 }
 
 void AjouterActionsetCredits(Player_s &Player) // a chaque action // OK
@@ -90,8 +101,9 @@ void AchatObjet(vector <Inventaire_s> &marchand, int &indexvectormarchand, int &
 	{
 		Player.InventaireJoueur.push_back({ marchand[indexvectormarchand].Objet, QuantitePourAcheter });
 		Player.Credits -= marchand[indexvectormarchand].Objet.CreditCost*QuantitePourAcheter;
+		AjouterActionsetCredits(Player);
 	}
-	AjouterActionsetCredits(Player);
+	
 }
 
 void UtiliserObjet(Player_s& Player, int& indexvectorplayer, int& QuantitePourUtiliser)
@@ -106,6 +118,18 @@ void UtiliserObjet(Player_s& Player, int& indexvectorplayer, int& QuantitePourUt
 		}
 		AjouterActionsetCredits(Player);
 	}
+}
+
+void AfficherJoueur(Player_s& Player)
+{
+	cout << "\n++ INFORMATIONS JOUEUR ++\n\n"
+		"CREDITS: " << Player.Credits;
+		cout<< "\nNOM OBJET\tQUANTITE\n";
+	for (int i = 0; i < Player.InventaireJoueur.size(); i++)
+	{
+		cout<< Player.InventaireJoueur[i].Objet.NomObjet <<"\t\t"<< Player.InventaireJoueur[i].QuantiteObjet<<"\n";
+	}
+
 }
 
 
